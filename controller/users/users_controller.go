4d4048bcdde2886af8eb4fa34076fec7be8277ca
Cristian <cgacitua@acid.cl>
2020-04-13 21:57:53 -0400

    configure model and utils errors

--- controller/users/users_controller.go
+++ controller/users/users_controller.go
@@ -1,8 +1,11 @@
 package users
 
 import (
+	"github.com/CriGacituaFlores/bookstore_users-api/utils/errors"
+	"github.com/CriGacituaFlores/bookstore_users-api/services"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"github.com/CriGacituaFlores/bookstore_users-api/domain/users"
 )
 
 var(counter int)
@@ -12,6 +15,20 @@ func GetUser(c *gin.Context) {
 }
 
 func CreateUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented, "implement me!")
+	var user users.User
+
+	if err := c.ShouldBindJSON(&user); err != nil {
+		restErr := errors.NewBadRequestError("Invalid json body")
+		c.JSON(restErr.Status, restErr)
+		return
+	}
+
+	result, saveErr := services.CreateUser(user)
+	if saveErr != nil {
+		c.JSON(saveErr.Status, saveErr)
+		return
+	}
+
+	c.JSON(http.StatusCreated, result)
 }
 
--- services/user_services.go
+++ services/user_services.go
@@ -1,1 +1,10 @@
 package services
+
+import (
+	"github.com/CriGacituaFlores/bookstore_users-api/utils/errors"
+	"github.com/CriGacituaFlores/bookstore_users-api/domain/users"
+)
+
+func CreateUser(user users.User) (*users.User, *errors.RestErr) {
+	return &user, nil
+}
